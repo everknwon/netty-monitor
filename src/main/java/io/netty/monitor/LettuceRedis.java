bc3e6239eeb3272bd724e0c98d742bebf4b38dfa
1619kHz <37778292+everknwon@users.noreply.github.com>
2020-05-11 00:28:58 +0800

    feat: 增加redis配置项，支持通用泛型RedisCodec

--- src/main/java/io/netty/monitor/LettuceRedis.java
+++ src/main/java/io/netty/monitor/LettuceRedis.java
@@ -26,36 +26,130 @@ package io.netty.monitor;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
 import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.codec.RedisCodec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
-public class LettuceRedis {
+import static io.netty.monitor.NettyMonitor.requireState;
+import static java.util.Objects.requireNonNull;
+
+public class LettuceRedis<K, V> implements RedisCodec<K, V> {
   private static final Logger log = LoggerFactory.getLogger(LettuceRedis.class);
 
-  private static final LettuceRedis instance = new LettuceRedis();
-  private final RedisURI redisUri = RedisURI.builder()
-          .withHost("localhost").withPort(6379)
+  private final String withHost = "localhost";
+  private final Integer withPort = 6379;
+
+  private RedisURI redisUri = RedisURI.builder().withHost(withHost).withPort(withPort)
           .withTimeout(Duration.of(10, ChronoUnit.SECONDS)).build();
 
   private final RedisClient redisClient = RedisClient.create(redisUri);
-  private final StatefulRedisConnection<String, String> connection = redisClient.connect();
-  private final RedisCommands<String, String> redisCommands = connection.sync();
+  private StatefulRedisConnection<K, V> connection;
 
-  private LettuceRedis(){}
+  private LettuceRedis() {}
 
-  public static LettuceRedis getRedisHelper() {
-    return Objects.requireNonNull(instance);
+  private LettuceRedis(RedisURI redisURI) {
+    requireNonNull(redisURI, "Redis uri configuration is required.");
+    requireState(redisURI.getPort() >= 0 && redisURI.getPort() <= 65533,
+            "Port number must be available: %s", redisURI.getPort());
+    Integer port = Objects.requireNonNullElse(redisURI.getPort(), withPort);
+    redisURI.setPort(port);
+    this.redisUri = redisURI;
   }
 
-  private void release() {
+  public static <K, V> LettuceRedis<K, V> newBuilder() {
+    return new LettuceRedis<>();
+  }
+
+  public static <K, V> LettuceRedis<K, V> newBuilder(RedisURI redisURI) {
+    return new LettuceRedis<>(redisURI);
+  }
+
+  public RedisCommands<K, V> build() {
+    this.connection = redisClient.connect(this);
+    return connection.sync();
+  }
+
+  public RedisAsyncCommands<K, V> buildAsync() {
+    this.connection = redisClient.connect(this);
+    return connection.async();
+  }
+
+  public RedisReactiveCommands<K, V> buildReactive() {
+    this.connection = redisClient.connect(this);
+    return connection.reactive();
+  }
+
+  public void release() {
     if (!Objects.isNull(connection)) {
       this.connection.close();
       this.redisClient.shutdown();
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public K decodeKey(ByteBuffer byteBuffer) {
+    return (K) decodeByteBuffer(byteBuffer);
+  }
+
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public V decodeValue(ByteBuffer byteBuffer) {
+    return (V) decodeByteBuffer(byteBuffer);
+  }
+
+  @Override
+  public ByteBuffer encodeKey(K k) {
+    return encodeByteBuffer(k);
+  }
+
+  @Override
+  public ByteBuffer encodeValue(V v) {
+    return encodeByteBuffer(v);
+  }
+
+  private ByteBuffer encodeByteBuffer(Object o) {
+    try (ByteArrayOutputStream byteArrayOps =
+                 new ByteArrayOutputStream();
+         ObjectOutputStream objectOps =
+                 new ObjectOutputStream(byteArrayOps)) {
+      objectOps.writeObject(o);
+      long freeMemory = Runtime.getRuntime().freeMemory();
+      if (byteArrayOps.size() > freeMemory) {
+        throw new OutOfMemoryError("ByteBuffer needs to allocate more memory than freeMemory");
+      }
+      ByteBuffer byteBuffer = ByteBuffer.allocateDirect(byteArrayOps.size());
+      byteBuffer.put(byteArrayOps.toByteArray());
+      return byteBuffer;
+    } catch (Exception e) {
+      log.error("encode an exception occurs:{}", o, e);
+      return null;
+    }
+  }
+
+  private Object decodeByteBuffer(ByteBuffer byteBuffer) {
+    byte[] array = byteBuffer.array();
+    try (ByteArrayInputStream byteArrayIps =
+                 new ByteArrayInputStream(array);
+         ObjectInputStream objectIps =
+                 new ObjectInputStream(byteArrayIps)) {
+      return objectIps.readObject();
+    } catch (Exception e) {
+      log.error("decode an exception occurs:{}", byteBuffer, e);
+      return null;
     }
   }
 }
